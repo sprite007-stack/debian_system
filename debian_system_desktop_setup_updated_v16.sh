50f8ee0bbd2614cb6c6e242778cab617667a7385
sprite007-stack <bryan.stepney+github@gmail.com>
2026-04-13 13:55:15 -0400

    Delete debian_system_desktop_setup_updated_v16.sh

--- debian_system_desktop_setup_updated_v16.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Placeholder for debian_system_desktop_setup_updated_v16.sh
-# NOTE: This file is a regenerated placeholder due to download issue.
-# Please reapply Section 1 fix in your existing script as provided.
-
-echo 'This is a placeholder v16 script. Apply Section 1 fix manually as instructed.'
